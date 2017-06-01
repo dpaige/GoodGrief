e7e75240ebeffadfac4c3ec474e4d387225beed8
Dave Paige <DavidBPaige@gmail.com>
2017-06-01 14:02:17 -0500

    Add simple first page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EntrancePage } from '../entrance/entrance'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  entrancePage = EntrancePage;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+  }
+
+  openItem() {
+    this.navCtrl.push(EntrancePage);
   }
 
 }
